5ecb4e0a6ed4d07517fdfb4f669a21ace6973439
Crazy Pioneer <35123708+developer1031@users.noreply.github.com>
2024-04-04 05:59:20 +0900

    Update HelpCreateRequest.php

--- modules/Helps/Http/Requests/HelpCreateRequest.php
+++ modules/Helps/Http/Requests/HelpCreateRequest.php
@@ -6,28 +6,28 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class HelpCreateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'title' => 'required',
-            'capacity' => 'integer|min:0',
-            'amount' => 'required|numeric|min:0',
-            'payment_method' => 'required|in:credit,crypto',
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'title' => 'required',
+      'capacity' => 'integer|min:0',
+      'amount' => 'required|numeric|min:0',
+      'payment_method' => 'required|in:credit,crypto,both',
+    ];
+  }
 }
