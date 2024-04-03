f536eb4b4abcd73a1257dfb9a3bda6eb78ff4920
Crazy Pioneer <35123708+developer1031@users.noreply.github.com>
2024-04-04 08:10:45 +0900

    fix

--- modules/Search/Domain/Queries/HangoutQuery.php
+++ modules/Search/Domain/Queries/HangoutQuery.php
@@ -12,138 +12,138 @@ use Modules\Search\Traits\Searchable;
 
 class HangoutQuery
 {
-    use Searchable;
+  use Searchable;
 
-    public function execute()
-    {
-        $skills = [];
-        $categories = [];
+  public function execute()
+  {
+    $skills = [];
+    $categories = [];
 
-        if($this->query) {
-            //$skills = Skill::where('name', 'like', '%'.$this->query.'%')->get()->pluck('id')->toArray();
-            $categories = Category::where('name', 'like', '%'.$this->query.'%')->get()->pluck('id')->toArray();
-        }
+    if ($this->query) {
+      //$skills = Skill::where('name', 'like', '%'.$this->query.'%')->get()->pluck('id')->toArray();
+      $categories = Category::where('name', 'like', '%' . $this->query . '%')->get()->pluck('id')->toArray();
+    }
 
-        if($this->category) {
-            //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
-            $categories = $this->category;
-        }
+    if ($this->category) {
+      //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
+      $categories = $this->category;
+    }
 
-        if(app('request')->has('skills')) {
-            $skills = app('request')->input('skills');
-            //$skills = Skill::where('name', 'like', '%'.$skill.'%')->get()->pluck('id')->toArray();
-        }
+    if (app('request')->has('skills')) {
+      $skills = app('request')->input('skills');
+      //$skills = Skill::where('name', 'like', '%'.$skill.'%')->get()->pluck('id')->toArray();
+    }
 
-        //$age    =   app('request')->input('age');
-        //$gender =   app('request')->input('gender');
-        if($this->location) {
+    //$age    =   app('request')->input('age');
+    //$gender =   app('request')->input('gender');
+    if ($this->location) {
+    } else {
+    }
+    $sql    =   DB::table('hangout_hangouts as hangouts')
+      ->select(
+        'hangouts.id as hangout_id',
+        'hangouts.type as hangout_type',
+        'hangouts.title as hangout_title',
+        'hangouts.description as hangout_description',
+        'hangouts.start as hangout_start',
+        'hangouts.end as hangout_end',
+        'hangouts.schedule as hangout_schedule',
+        'hangouts.capacity as hangout_capacity',
+        'hangouts.available as hangout_available',
+        'hangouts.kizuna as hangout_kizuna',
+        'hangouts.created_at as hangout_created_at',
+        'hangouts.updated_at as hangout_updated_at',
+        'hangouts.available_status as available_status',
+        'hangouts.is_fake as is_fake',
+        'hangouts.cover_img as cover_img',
+        'hangout_uploads.thumb as hangout_cover_thumb',
+        'hangout_uploads.path as hangout_cover_path',
+        'users.id as user_id',
+        'users.name as user_name',
+        'user_uploads.thumb as user_avatar',
+        'users.social_avatar as social_avatar',
+        'hangouts.is_range_price as hangout_is_range_price',
+        'hangouts.min_amount as hangout_min_amount',
+        'hangouts.max_amount as hangout_max_amount',
+        'hangouts.amount as hangout_amount',
+        'hangouts.payment_method as payment_method'
+      )
+      ->join('users', 'users.id', '=', 'hangouts.user_id')
+      ->leftJoin('uploads as user_uploads', 'user_uploads.id', '=', 'users.avatar_id')
+      ->leftJoin('uploads as hangout_uploads', 'hangout_uploads.uploadable_id', '=', 'hangouts.id')
+      ->leftJoin('skillables as sk', 'sk.skillable_id', '=', 'hangouts.id')
+      ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'hangouts.id')
+      ->whereNull('hangouts.deleted_at');
+    // ->where(function($query) {
+    //     //$query->where('hangouts.start', '>', Carbon::now());
+    //     //$query->orWhereNull('hangouts.start');
+    // });
 
-        } else {
-            
-        }
-        $sql    =   DB::table('hangout_hangouts as hangouts')
-                        ->select(
-                            'hangouts.id as hangout_id',
-                            'hangouts.type as hangout_type',
-                            'hangouts.title as hangout_title',
-                            'hangouts.description as hangout_description',
-                            'hangouts.start as hangout_start',
-                            'hangouts.end as hangout_end',
-                            'hangouts.schedule as hangout_schedule',
-                            'hangouts.capacity as hangout_capacity',
-                            'hangouts.available as hangout_available',
-                            'hangouts.kizuna as hangout_kizuna',
-                            'hangouts.created_at as hangout_created_at',
-                            'hangouts.updated_at as hangout_updated_at',
-                            'hangouts.available_status as available_status',
-                            'hangouts.is_fake as is_fake',
-                            'hangouts.cover_img as cover_img',
-                            'hangout_uploads.thumb as hangout_cover_thumb',
-                            'hangout_uploads.path as hangout_cover_path',
-                            'users.id as user_id',
-                            'users.name as user_name',
-                            'user_uploads.thumb as user_avatar',
-                            'users.social_avatar as social_avatar',
-                            'hangouts.is_range_price as hangout_is_range_price',
-                            'hangouts.min_amount as hangout_min_amount',
-                            'hangouts.max_amount as hangout_max_amount',
-                            'hangouts.amount as hangout_amount',
-                            'hangouts.payment_method as payment_method'
-                        )
-                        ->join('users', 'users.id', '=', 'hangouts.user_id')
-                        ->leftJoin('uploads as user_uploads', 'user_uploads.id', '=', 'users.avatar_id')
-                        ->leftJoin('uploads as hangout_uploads', 'hangout_uploads.uploadable_id', '=', 'hangouts.id')
-                        ->leftJoin('skillables as sk', 'sk.skillable_id', '=', 'hangouts.id')
-                        ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'hangouts.id')
-                        ->whereNull('hangouts.deleted_at');
-                        // ->where(function($query) {
-                        //     //$query->where('hangouts.start', '>', Carbon::now());
-                        //     //$query->orWhereNull('hangouts.start');
-                        // });
+    $age = app('request')->input('age');
+    $gender =   app('request')->input('gender');
 
-        $age = app('request')->input('age');
-        $gender =   app('request')->input('gender');
+    if ($this->amount) {
+      //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
+      $sql->where('hangouts.amount', $this->amount);
+    }
+    if ($this->minAmount) {
+      $sql->whereRaw('hangouts.min_amount >= ' . $this->minAmount . ' OR hangouts.amount >= ' . $this->minAmount);
+    }
 
-        if($this->amount) {
-            //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
-            $sql->where('hangouts.amount', $this->amount);
-        }
-        if($this->minAmount) {
-            $sql->whereRaw('hangouts.min_amount >= ' . $this->minAmount . ' OR hangouts.amount >= ' . $this->minAmount);
-        }
+    if ($this->maxAmount) {
+      $sql->whereRaw('hangouts.max_amount <=' . $this->maxAmount . ' OR hangouts.amount <= ' . $this->maxAmount);
+    }
 
-        if($this->maxAmount) {
-            $sql->whereRaw('hangouts.max_amount <=' . $this->maxAmount . ' OR hangouts.amount <= ' . $this->maxAmount);
-        }
+    if ($this->paymentMethod) {
+      //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
+      $sql->where('hangouts.payment_method', $this->paymentMethod);
+    }
 
-        if($this->paymentMethod) {
-            //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
-            $sql->where('hangouts.payment_method', $this->paymentMethod);
-        }
+    if ($this->offerType) {
+      //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
+      if ($this->offerType != 'all') {
 
-        if($this->offerType) {
-            //$categories = Category::where('name', 'like', '%'.$this->category.'%')->get()->pluck('id')->toArray();
-            $sql->where('hangouts.type', $this->offerType);
-        }
+        $sql->where('hangouts.type', $this->offerType);
+      }
+    }
 
-        if($age || $gender) {
-            $sql->whereRaw("1 <> 1");
-        }
-        else {
-            if($this->query) {
-                $sql->where('hangouts.title', 'like', '%'.$this->query.'%');
-            }
+    if ($age || $gender) {
+      $sql->whereRaw("1 <> 1");
+    } else {
+      if ($this->query) {
+        $sql->where('hangouts.title', 'like', '%' . $this->query . '%');
+      }
 
-            if(app('request')->has('skills') || $categories) {
-                $sql->where(function($query) use ($skills, $categories) {
-                    //$query->where('hangouts.title', 'like', '%'.$this->query.'%');
-                    //$query->orwhere('users.name', 'like', '%'.$this->query.'%');
-                    //$query->orWhere('users.email', 'like', '%'.$this->query.'%');
-                    if($skills) {
-                        $query->whereIn('sk.skill_id', $skills);
-                    }
-                    if($categories) {
-                        $query->whereIn('category.category_id', $categories);
-                    }
-                });
-            }
-            if(app('request')->has('available_status')) {
-                $sql->whereIn('hangouts.available_status', ['online', 'combine']);
-            }
+      if (app('request')->has('skills') || $categories) {
+        $sql->where(function ($query) use ($skills, $categories) {
+          //$query->where('hangouts.title', 'like', '%'.$this->query.'%');
+          //$query->orwhere('users.name', 'like', '%'.$this->query.'%');
+          //$query->orWhere('users.email', 'like', '%'.$this->query.'%');
+          if ($skills) {
+            $query->whereIn('sk.skill_id', $skills);
+          }
+          if ($categories) {
+            $query->whereIn('category.category_id', $categories);
+          }
+        });
+      }
+      if (app('request')->has('available_status')) {
+        $sql->whereIn('hangouts.available_status', ['online', 'combine']);
+      }
 
-            // $sql->where(function($query1) {
-            //     $query1->whereDate('hangouts.end', '>=', Carbon::now());
-            //     $query1->orWhereNull('hangouts.end');
-            // });
-            // $sql->where(function($query1) {
-            //     $query1->whereDate('hangouts.start', '>=', Carbon::now());
-            //     $query1->orWhereNull('hangouts.start');
-            // });
+      // $sql->where(function($query1) {
+      //     $query1->whereDate('hangouts.end', '>=', Carbon::now());
+      //     $query1->orWhereNull('hangouts.end');
+      // });
+      // $sql->where(function($query1) {
+      //     $query1->whereDate('hangouts.start', '>=', Carbon::now());
+      //     $query1->orWhereNull('hangouts.start');
+      // });
 
-            //$sql->whereNull('hangouts.deleted');
-            //$sql->whereNull('hangouts.room_id');
+      //$sql->whereNull('hangouts.deleted');
+      //$sql->whereNull('hangouts.room_id');
 
-            /*
+      /*
             if ($age) {
                 $ageRange = explode('-', $age);
                 if( count($ageRange) > 1) {
@@ -157,14 +157,14 @@ class HangoutQuery
             }
             */
 
-            $sql->where('hangouts.is_completed', 0);
-            $sql->whereNull('hangouts.deleted_at');
-            
-            $sql->orderBy('hangouts.created_at', 'desc')
-                //->groupBy('hangouts.id')
-                ->groupBy('hangouts.title');
-        }
+      $sql->where('hangouts.is_completed', 0);
+      $sql->whereNull('hangouts.deleted_at');
 
-        return $sql->paginate($this->perPage);
+      $sql->orderBy('hangouts.created_at', 'desc')
+        //->groupBy('hangouts.id')
+        ->groupBy('hangouts.title');
     }
+
+    return $sql->paginate($this->perPage);
+  }
 }
--- modules/Search/Domain/Queries/UserQuery.php
+++ modules/Search/Domain/Queries/UserQuery.php
@@ -10,126 +10,128 @@ use Modules\Search\Traits\Searchable;
 
 class UserQuery
 {
-    use Searchable;
+  use Searchable;
 
-    public function execute()
-    {
-        if(app('request')->has('available_status') || $this->category) {
-            $sql = DB::table('users')
-                ->select(
-                    'users.id as user_id',
-                    'users.name as user_name',
-                    'users.about as user_about',
-                    'users.birth_date as user_birth_date',
-                    'users.gender as user_gender',
-                    'locations.address as user_location_address',
-                    'locations.lat as user_location_lat',
-                    'locations.lng as user_location_lng',
-                    'uploads.thumb as user_thumb',
-                    'uploads.path as user_path',
-                    'users.social_avatar as social_avatar',
-                    'users.language as user_language'
-                )
-                ->leftJoin('uploads', 'uploads.id', '=', 'users.avatar_id')
-                ->leftJoin('locations', 'users.id', '=', 'locations.locationable_id')
-                ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'users.id')
-                ->leftJoin('skillables as skill', 'skill.skillable_id', '=', 'users.id');
+  public function execute()
+  {
+    if (app('request')->has('available_status') || $this->category) {
+      $sql = DB::table('users')
+        ->select(
+          'users.id as user_id',
+          'users.name as user_name',
+          'users.about as user_about',
+          'users.birth_date as user_birth_date',
+          'users.gender as user_gender',
+          'locations.address as user_location_address',
+          'locations.lat as user_location_lat',
+          'locations.lng as user_location_lng',
+          'uploads.thumb as user_thumb',
+          'uploads.path as user_path',
+          'users.social_avatar as social_avatar',
+          'users.language as user_language'
+        )
+        ->leftJoin('uploads', 'uploads.id', '=', 'users.avatar_id')
+        ->leftJoin('locations', 'users.id', '=', 'locations.locationable_id')
+        ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'users.id')
+        ->leftJoin('skillables as skill', 'skill.skillable_id', '=', 'users.id');
 
-            $sql->groupBy('users.id');
-            $sql->whereDate('users.created_at', '<', '1990-01-01');
+      $sql->groupBy('users.id');
+      $sql->whereDate('users.created_at', '<', '1990-01-01');
+    } else {
+      // User specific search
+      $age    =   app('request')->input('age');
+      $gender =   app('request')->input('gender');
+      $language =   app('request')->input('language');
+
+      $categories = [];
+      $skills = [];
+      if ($this->category) {
+        //$categories = Category::where('name', 'like', '%' . $this->category . '%')->get()->pluck('id')->toArray();
+        $categories = $this->category;
+      }
+
+      if (app('request')->has('skills')) {
+        //$skill = app('request')->input('skills');
+        $skills = app('request')->input('skills');
+
+        //$skills = Skill::where('name', 'like', '%'.$skill.'%')->get()->pluck('id')->toArray();
+        //$skills = Skill::whereIn('id', $skill)->get()->pluck('id')->toArray();
+      }
+
+      $sql = DB::table('users')
+        ->select(
+          'users.id as user_id',
+          'users.name as user_name',
+          'users.about as user_about',
+          'users.birth_date as user_birth_date',
+          'users.gender as user_gender',
+          'locations.address as user_location_address',
+          'locations.lat as user_location_lat',
+          'locations.lng as user_location_lng',
+          'uploads.thumb as user_thumb',
+          'uploads.path as user_path',
+          'users.social_avatar as social_avatar',
+          'users.language as user_language'
+        )
+        ->leftJoin('uploads', 'uploads.id', '=', 'users.avatar_id')
+        ->leftJoin('locations', 'users.id', '=', 'locations.locationable_id')
+        ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'users.id')
+        ->leftJoin('skillables as skill', 'skill.skillable_id', '=', 'users.id');
+
+      $sql->where('users.id', '!=', auth()->user()->id)
+        ->whereNull('users.deleted')
+        ->where(function ($query) {
+          $query->where('users.admin', false);
+          $query->orWhereNull('users.admin');
+        });
+
+      if ($this->query) {
+        $sql = $sql->where(function ($query) {
+          $query->where('users.name', 'like', $this->query . '%');
+          //$query->orWhere('users.email', 'like', '%'.$this->query.'%');
+        });
+      }
+
+      if ($categories) {
+        $sql->where(function ($query) use ($categories) {
+          $query->whereIn('category.category_id', $categories);
+        });
+      }
+
+      if ($skills) {
+        $sql->where(function ($query) use ($skills) {
+          $query->whereIn('skill.skill_id', $skills);
+        });
+      }
+
+      if ($age) {
+        if ($age == 'all') {
+        } else {
+          $ageRange = explode('-', $age);
+          if (count($ageRange) > 1) {
+            $ageMin   = $ageRange[0];
+            $ageMax   = $ageRange[1];
+            $sql->whereBetween('users.age', [$ageMin, $ageMax]);
+          }
         }
-        else {
-            // User specific search
-            $age    =   app('request')->input('age');
-            $gender =   app('request')->input('gender');
-            $language =   app('request')->input('language');
+      }
 
-            $categories = [];
-            $skills = [];
-            if($this->category) {
-                //$categories = Category::where('name', 'like', '%' . $this->category . '%')->get()->pluck('id')->toArray();
-                $categories = $this->category;
-            }
+      if ($gender) {
+        $sql->where('users.gender', $gender);
+      }
 
-            if(app('request')->has('skills')) {
-                //$skill = app('request')->input('skills');
-                $skills = app('request')->input('skills');
+      if ($language) {
+        $sql = $sql->where(function ($query) use ($language) {
+          $query->where('users.language', 'like', '%' . $language . '%');
+        });
+      }
 
-                //$skills = Skill::where('name', 'like', '%'.$skill.'%')->get()->pluck('id')->toArray();
-                //$skills = Skill::whereIn('id', $skill)->get()->pluck('id')->toArray();
-            }
-
-            $sql = DB::table('users')
-                ->select(
-                    'users.id as user_id',
-                    'users.name as user_name',
-                    'users.about as user_about',
-                    'users.birth_date as user_birth_date',
-                    'users.gender as user_gender',
-                    'locations.address as user_location_address',
-                    'locations.lat as user_location_lat',
-                    'locations.lng as user_location_lng',
-                    'uploads.thumb as user_thumb',
-                    'uploads.path as user_path',
-                    'users.social_avatar as social_avatar',
-                    'users.language as user_language'
-                )
-                ->leftJoin('uploads', 'uploads.id', '=', 'users.avatar_id')
-                ->leftJoin('locations', 'users.id', '=', 'locations.locationable_id')
-                ->leftJoin('categoryables as category', 'category.categoryable_id', '=', 'users.id')
-                ->leftJoin('skillables as skill', 'skill.skillable_id', '=', 'users.id');
-
-            $sql->where('users.id', '!=', auth()->user()->id)
-                ->whereNull('users.deleted')
-                ->where(function($query) {
-                    $query->where('users.admin', false);
-                    $query->orWhereNull('users.admin');
-                });
-
-            if($this->query) {
-                $sql = $sql->where(function($query) {
-                    $query->where('users.name', 'like', $this->query.'%');
-                    //$query->orWhere('users.email', 'like', '%'.$this->query.'%');
-                });
-            }
-
-            if($categories) {
-                $sql->where(function($query) use ($categories) {
-                    $query->whereIn('category.category_id', $categories);
-                });
-            }
-
-            if($skills) {
-                $sql->where(function($query) use ($skills) {
-                    $query->whereIn('skill.skill_id', $skills);
-                });
-            }
-
-            if ($age) {
-                $ageRange = explode('-', $age);
-                if( count($ageRange) > 1) {
-                    $ageMin   = $ageRange[0];
-                    $ageMax   = $ageRange[1];
-                    $sql->whereBetween('users.age', [$ageMin, $ageMax]);
-                }
-            }
-
-            if ($gender) {
-                $sql->where('users.gender', $gender);
-            }
-
-            if ($language) {
-                $sql = $sql->where(function($query) use ($language) {
-                    $query->where('users.language', 'like', '%'. $language .'%');
-                });
-            }
-
-            //No query Fake user
-            $sql->where('users.is_fake', '<>', 1);
-            $sql->groupBy('users.id');
-        }
-
-
-        return $sql->paginate($this->perPage);
+      //No query Fake user
+      $sql->where('users.is_fake', '<>', 1);
+      $sql->groupBy('users.id');
     }
+
+
+    return $sql->paginate($this->perPage);
+  }
 }
