6c2ca5bec152b0b3c787300281faec064b162d1b
developer <crazpioneer@gmail.com>
2024-06-17 23:01:08 +0900

    Update share-index.js

--- share-index.js
+++ share-index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 9876;
 
 app.use(cors());
 
+const BASEURL = "https://kizuner-st.inapps.technology/api/share/"
 const LinkAndroid = "https://play.google.com/store/apps/details?id=com.kizuner"
 const LinkIPhone = "https://apps.apple.com/us/app/kizuner/id1524617131"
 
@@ -23,65 +24,74 @@ app.get("/", (req, res) => {
 });
 
 const buildShareContent = function (data) {
-    return (
-        "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-        '<meta property="fb:app_id" content="1239520603104986" />' +
-        '<meta property="al:android:package" content="com.kizuner" />' +
-        '<meta property="al:android:app_name" content="Kizuner" />' +
-        '<meta property="al:android:url" content="' +
-        LinkAndroid +
-        '" />' +
-        '<meta property="al:ios:url" content="' +
-        LinkIPhone +
-        '" />' +
-        '<meta property="al:ios:app_store_id" content="1524617131" />' +
-        '<meta property="al:ios:app_name" content="Kizuner" />' +
-        '<meta property="al:web:should_fallback" content="true" />' +
-        '<meta property="al:web:url" content="https://kizuner.com" />' + // dont know if this is required
-        '<meta property="og:title" content="' +
-        data.title +
-        '" />' +
-        '<meta property="og:description" content="' +
-        data.description +
-        '" />' +
-        '<meta property="og:image" content="' +
-        data.image_url +
-        '" />' +
-        '<meta property="og:image:width" content="1200" />' +
-        '<meta property="og:image:height" content="630" />' +
-        '<meta property="og:image:type" content="image/jpeg" />' +
-        '<meta name="twitter:card" content="summary_large_image" />' +
-        '<meta name="twitter:site" content="@kizuner" />' +
-        '<meta name="twitter:creator" content="@kizuner" />' +
-        '<meta name="twitter:title" content="' +
-        data.title +
-        '" />' +
-        '<meta name="twitter:description" content="' +
-        data.description +
-        '" />' +
-        '<meta name="twitter:image" content="' +
-        data.image_url +
-        '" />' +
-        "</head>" +
-        "<body>" +
-        "<script>" +
-        'var userAgent = navigator.userAgent || navigator.vendor || window.opera;' +
-        'window.location = "' +
-        '(/iPhone/.test(userAgent) && !window.MSStream) ?' + LinkIPhone + ' : ' + LinkAndroid +
-        '"' +
-        "</script>" +
-        "</body>" +
-        "</html>"
-    );
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta property="fb:app_id" content="1239520603104986" />
+            <meta property="al:android:package" content="com.kizuner" />
+            <meta property="al:android:app_name" content="Kizuner" />
+            <meta property="al:android:url" content="${LinkAndroid}" />
+            <meta property="al:ios:url" content="${LinkIPhone}" />
+            <meta property="al:ios:app_store_id" content="1524617131" />
+            <meta property="al:ios:app_name" content="Kizuner" />
+            <meta property="al:web:should_fallback" content="true" />
+            <meta property="al:web:url" content="https://kizuner.com" />
+            <meta property="og:title" content="${data.title}" />
+            <meta property="og:description" content="${data.description}" />
+            <meta property="og:image" content="${data.image_url}" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content="@kizuner" />
+            <meta name="twitter:title" content="${data.title}" />
+            <meta name="twitter:description" content="${data.description}" />
+            <meta name="twitter:image" content="${data.image_url}" />
+            <title>${data.title}</title>
+        </head>
+        <body>
+            <script>
+                function getMobileOperatingSystem() {
+                    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+                    if (/windows phone/i.test(userAgent)) {
+                        return "Windows Phone";
+                    }
+                    if (/android/i.test(userAgent)) {
+                        return "Android";
+                    }
+                    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                        return "iOS";
+                    }
+                    return "unknown";
+                }
+                
+                window.onload = function() {
+                    const os = getMobileOperatingSystem();
+                    const storeUrl = {
+                        Android: 'https://play.google.com/store/apps/details?id=com.kizuner',
+                        iOS: 'https://apps.apple.com/app/id1524617131'
+                    };
+
+                    setTimeout(() => {
+                        if (os === 'Android' || os === 'iOS') {
+                            window.location = storeUrl[os];
+                        }
+                    }, 25);
+
+                    window.location = ${dynamicLink};
+                };
+            </script>
+        </body>
+        </html>
+    `;
 };
 
 app.get("/k", (req, res) => {
     const { t, d, i, k, id } = req.query;
 
-    var dynamicLink = LinkIPhone
-
+    var dynamicLink = `${BASEURL}/${k}/${id}`
     const title = decodeURIComponent(t) || "Do What You Love - Kizuner";
     const description = decodeURIComponent(d);
     const image_id = decodeURIComponent(i);
