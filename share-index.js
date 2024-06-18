cf69a34597ea5651bb9cb34941f09c9e4d2104ce
developer <crazpioneer@gmail.com>
2024-06-19 08:28:39 +0900

    Update share-index.js

--- share-index.js
+++ share-index.js
@@ -43,7 +43,8 @@ const buildShareContent = function (data) {
             <meta property="og:image" content="${data.image_url}" />
             <meta property="og:image:url" content="${data.image_url}" />
             <meta property="og:image:secure_url" content="${data.image_url}" />
-            
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
             <meta property="og:image:type" content="image/jpeg" /> 
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:creator" content="@kizuner" />
